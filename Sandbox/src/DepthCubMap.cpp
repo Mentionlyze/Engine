d76a84908b3494d0b975f9d64b391de0b8d13ce8
sunmengxin <mentionlyze@outlook.com>
2023-04-04 18:00:54 +0800

    fix

---
+++ Sandbox/src/DepthCubMap.cpp
@@ -0,0 +1,17 @@
+#include "DepthCubMap.h"
+
+DepthCubMap::DepthCubMap()
+{
+}
+
+void DepthCubMap::OnUpdate(Engine::Timestep ts)
+{
+}
+
+void DepthCubMap::OnEvent(Engine::Event& e)
+{
+}
+
+void DepthCubMap::OnImGuiRender()
+{
+}
